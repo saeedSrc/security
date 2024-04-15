b680c2f9d56f541c62ee725f6006ba2708efee6b
saeed rasooli <saeed_rasooli@yahoo.com>
2024-04-15 15:47:24 +0330

    create systems and leaders path

--- src/main/java/com/saeed/springboot/security/controller/DemoController.java
+++ src/main/java/com/saeed/springboot/security/controller/DemoController.java
@@ -10,4 +10,15 @@ public class DemoController {
     public String showHome() {
         return "home";
     }
+
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
 }
